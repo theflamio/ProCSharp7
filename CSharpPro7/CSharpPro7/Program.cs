68040b388ed4e282d4ff834b65aa9ed08f01159a
Flemming_Christensen <flamekfor@gmail.com>
2019-09-04 22:21:25 +0200

    first

---
+++ CSharpPro7/CSharpPro7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpPro7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
